efadd778cfa33286e5767cccde5e5c00c7bff611
Joe Knight <joe@stream.me>
2016-12-07 17:01:47 -0600

    Basic testing framework added

---
+++ test/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by joe on 12/7/16.
+ */
+
+var assert = require('assert');
+
+var de
+
+describe('npme-recovery-tool', function() {
+    it('should work, maybe?', function(){
+        assert.equal(true, true)
+    })
+})
